2025b417a398ae9ad95f97e2b7edca617d31cd69
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-02-17 16:15:18 -0500

    Simplifying visuals (at least temporarily)

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 let playerDeck = [];
 let opponentDeck = [];
 let roundCards = [];
+const div = document.createElement("div")
 
 
 
@@ -19,7 +20,6 @@ class fullDeck {
         this.deck = [];         
         this.sort();            // Calling this actually creates the deck
         this.shuffle();         // Since we need a shuffled deck anyway, this saves using the method later
-        this.createVisuals();
     }
 
     // Method that produces a full deck in order, as you might see in a brand new pack
@@ -27,7 +27,7 @@ class fullDeck {
     sort() {
         this.deck = [];
         this.names = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K',];
-        this.suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
+        this.suits = ['&spades;', '&hearts;', '&clubs;', '&diams;'];
         this.colors = ['black', 'red'];              // I think this key will help me when I create visuals
 
         // Creates a card of every name in each suit
@@ -44,181 +44,24 @@ class fullDeck {
                 card.value += 13
             }
         });
+
+
+        const deck = this.deck;
+        for (let i = 0; i < deck.length; i++) {                                  // Create a div for each card
+            let suitSymbol;
+            div.className = 'card';
+    
+            if (this.deck[i].suit == 'Diamonds') {                               // Generates the viewable symbols for each suit
+                suitSymbol = '&diams;';
+            } else {
+                suitSymbol = '&' + deck[i].suit.toLowerCase() + ';';
+            }
+    
+        div.innerHTML = '' + deck[i].name + '' + suitSymbol + '';           // Displays card name and suit
+        document.body.appendChild(document.createElement('div'));           // Make the div a child element of the body
+        }
     }
-
-    createVisuals() {
-        
-        const elementCreator = (tagName, attributes, children) => {
-            const element = document.createElement(tagName);
-            
-            if (attributes) {
-              for (const attrName in attributes) {
-                element.setAttribute(attrName, attributes[attrName]);
-              }
-            }
-            
-            if (children) {
-              for (let i = 0; i < children.length; i++) {
-                const child = children[i];
-                
-                if (typeof child === 'string') {
-                  element.appendChild(document.createTextNode(child));
-                } else {
-                  element.appendChild(child);
-                }
-              }
-            }
-            
-            return element;
-        };
-          
-        const div = (attributes, children) => elementCreator('div', attributes, children);
-        
-
-
-
-            const deck = this.deck;
-            // const div = document.createElement('div');
-            for (let i = 0; i < deck.length; i++) {                                  // Create a div for each card
-                let suitSymbol;
-                div.className = 'card';
-        
-                if (this.deck[i].suit == 'Diamonds') {                               // Generates the viewable symbols for each suit
-                    suitSymbol = '&diams;';
-                } else {
-                    suitSymbol = '&' + deck[i].suit.toLowerCase() + ';';
-                }
-        
-                div.innerHTML = '' + deck[i].name + '' + suitSymbol + '';           // Displays card name and suit
-                document.body.appendChild(document.createElement('div'));           // Make the div a child element of the body
-            }
-        
-            
-            const createSuit = (suit) => (position) => {
-                const [x, y, mirrored] = position;
-                let mirrorClass;
-                if (mirrored) {
-                    mirrorClass = " mirrored";
-                } else { mirrorClass = ""}
-            
-                return div({
-                    class: 'cardSuitSymbol' + mirrorClass,
-                    style: `left: ${x * 100}%; top: ${y * 100}%;`
-                }, [suit]);
-            };
-            
-
-            // This bit of code is the best way I found to show the suits on the card faces
-            // I didn't want to use an image for each card
-            // Format is shown in rows, with each array in the format:
-            // [x-position, y-position, upside-down]
-            const suitPositions = [
-                [
-                [0, 0]                                    // A
-                ],
-
-                [
-                [0, -1],
-                [0, 1, true]                              // 2
-                ],
-
-                [
-                [0, -1],
-                [0, 0],                                   // 3
-                [0, 1, true]
-                ],
-
-                [
-                [-1, -1], [1, -1],
-                [-1, 1, true], [1, 1, true]               // 4
-                ],
-
-                [
-                [-1, -1], [1, -1],
-                [0, 0],                                   // 5
-                [-1, 1, true], [1, 1, true]
-                ],
-
-                [
-                [-1, -1], [1, -1],
-                [-1, 0], [1, 0],                          // 6
-                [-1, 1, true], [1, 1, true]
-                ],
-
-                [
-                [-1, -1], [1, -1],
-                [0, -0.5],                                // 7
-                [-1, 0], [1, 0],
-                [-1, 1, true], [1, 1, true]
-                ],
-
-                [
-                [-1, -1], [1, -1],
-                [0, -0.5],
-                [-1, 0], [1, 0],                          // 8
-                [0, 0.5, true],
-                [-1, 1, true], [1, 1, true]
-                ],
-
-                [
-                [-1, -1], [1, -1],
-                [-1, -1 / 3], [1, -1 / 3],
-                [0, 0],                                   // 9
-                [-1, 1 / 3, true], [1, 1 / 3, true],
-                [-1, 1, true], [1, 1, true]
-                ],
-
-                [
-                [-1, -1], [1, -1],
-                [0, -2 / 3],
-                [-1, -1 / 3], [1, -1 / 3],
-                [-1, 1 / 3, true], [1, 1 / 3, true],      // 10
-                [0, 2 / 3, true],
-                [-1, 1, true], [1, 1, true]
-                ],
-
-                [
-                [0, 0]                                    // J
-                ],
-
-                [
-                [0, 0]                                    // Q
-                ],
-
-                [
-                [0, 0]                                    // K
-                ]
-            ];
-
-
-
-
-            deck.forEach((card) => {
-                const colorClass = 'card ' + card.color;
-        
-                return div({class: colorClass}, [
-                  div({ class: 'cardFaceSuits'},
-                    suitPositions[card.value % 13].map(createSuit(card.suit))        // ************NEED TO FIX********************
-                  ),
-                  div({ class: 'cardTopLeft'}, [
-                    div({ class: 'cardCornerName'}, [
-                     card.name
-                    ]),
-                    div({ class: 'cardCornerSuit' }, [
-                     card.suit
-                    ])
-                  ]),
-                  div({ class: 'cardBottomRight' }, [
-                    div({ class: 'cardCornerName' }, [
-                      card.name
-                    ]),
-                    div({ class: 'cardCornerSuit' }, [
-                      card.suit
-                    ])
-                  ])
-                ]);
-            })
-        };
+    
 
     // Method for shuffling (randomizing) the array of cards
     // I'm using the Fisher-Yates Shuffle algorithm for optimal randomization
@@ -261,16 +104,19 @@ gameDeck.dealHalf();
 
 
 
+
 //
 //------------------- GAMEPLAY ------------------------
 //
 
 const playRound = (playerDeck, opponentDeck) => {
     roundCards.push(playerDeck.shift());                    // NOTE: Player's card will always have an even index# in roundCards array
-    document.querySelector(".playerHand").innerHTML = roundCards[0]
+    document.querySelector(".card.player.obverse").innerHTML = ` ${roundCards[0].name} ${roundCards[0].suit} `;
+    document.querySelector(".card.player.obverse").className += ` ${roundCards[0].color}`;
 
     roundCards.push(opponentDeck.shift());
-
+    document.querySelector(".card.opponent.obverse").innerHTML = ` ${roundCards[1].name} ${roundCards[1].suit} `;
+    document.querySelector(".card.opponent.obverse").className += ` ${roundCards[1].color}`;
 
     // Test for a draw in the cards played and if so, deal out cards in a "war" scenario
     // The do-while loop will repeat until there is not a draw between the last played card by each side
@@ -301,7 +147,7 @@ const playRound = (playerDeck, opponentDeck) => {
 }
 
 
-
+playRound(playerDeck, opponentDeck);
 
 // STILL TO DO:
 //
